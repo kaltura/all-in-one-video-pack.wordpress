50855eb5d355d6d4a4b525e961d3b9304ad80a60
kaltura <kaltura@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-03-23 15:55:53 +0000

    added learn more text
    git-svn-id: http://svn.wp-plugins.org/all-in-one-video-pack/trunk@220958 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/kaltura_admin_partner_info_form.php
+++ admin/kaltura_admin_partner_info_form.php
@@ -80,7 +80,8 @@
 			</tr>
 			<tr class="kalturaLastRow">
 				<td colspan="2" align="left" style="padding-top: 10px;padding-left:10px">
-					<a href="http://www.kaltura.com/index.php/kmc" target="_blank">Login</a> to the Kaltura Management Console (KMC) for advanced <br />media management
+					<a href="http://www.kaltura.com/index.php/kmc" target="_blank">Login</a> to the Kaltura Management Console (KMC) for advanced <br />media management<br />
+					Learn More about <a href="http://wordpress.kaltura.org/" target="_blank">new plugin features</a>
 				</td>
 			</tr>
 		</table>
