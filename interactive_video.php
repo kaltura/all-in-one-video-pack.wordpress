254221767c7c434e5ac1d98b692305681831a054
kaltura <kaltura@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-05-09 09:08:13 +0000

    update version number in 2.4.1 tag
    git-svn-id: http://svn.wp-plugins.org/all-in-one-video-pack/tags/2.4.1@238855 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- interactive_video.php
+++ interactive_video.php
@@ -3,7 +3,7 @@
 Plugin Name: All in One Video Pack
 Plugin URI: http://kaltura.org/
 Description: This is not just another video embed tool - it includes every functionality you might need for video and rich-media, including playing, uploading and editing.  
-Version: 99.99(DEV)
+Version: 2.4.1
 Author: Kaltura
 Author URI: http://kaltura.org/
 */
