f4842bffb6074ac07124d8be15fcf1d3ea1eb9e2
roman-kreichman <roman.kreichman@gmail.com>
2015-05-17 14:19:34 +0300

    removed symbolic links workaround

--- all-in-one-video-pack.php
+++ all-in-one-video-pack.php
@@ -8,30 +8,10 @@ Author: Kaltura
 Author URI: http://www.kaltura.org/
 */
 
-define( 'KALTURA_PLUGIN_FILE_RESOLVED', __FILE__ );
+define( 'KALTURA_PLUGIN_FILE', __FILE__ );
 define( 'KALTURA_ROOT', dirname( __FILE__ ) );
 
 require_once( KALTURA_ROOT . '/lib/Kaltura/Autoloader.php' );
-
-// a workaround when using symbolic links and __FILE__ holds the resolved path
-$all_in_one_video_pack_file = KALTURA_PLUGIN_FILE_RESOLVED;
-if ( isset( $plugin ) ) {
-	if ( ! defined( 'KALTURA_PLUGIN_FILE' ) ) {
-		define( 'KALTURA_PLUGIN_FILE', $plugin );
-	}
-} elseif ( isset( $network_plugin ) ) {
-	if ( ! defined( 'KALTURA_PLUGIN_FILE' ) ) {
-		define( 'KALTURA_PLUGIN_FILE', $network_plugin );
-	}
-} elseif ( isset( $mu_plugin ) ) {
-	if ( ! defined( 'KALTURA_PLUGIN_FILE' ) ) {
-		define( 'KALTURA_PLUGIN_FILE', $mu_plugin );
-	}
-} else {
-	if ( ! defined( 'KALTURA_PLUGIN_FILE' ) ) {
-		define( 'KALTURA_PLUGIN_FILE', KALTURA_PLUGIN_FILE_RESOLVED );
-	}
-}
 
 $kalturaAutoloader = new Kaltura_Autoloader( plugin_dir_path( __FILE__ ) );
 $kalturaAutoloader->register();
