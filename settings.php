fac6c98161ff2cef618306ffb0bac156060b2310
Muli Dayan <muli.dayan@kaltura.com>
2016-08-24 12:04:28 +0300

    WP-92 tabs to spaces

--- settings.php
+++ settings.php
@@ -1,17 +1,17 @@
 <?php
 if ( ! defined( 'ABSPATH' ) ) {// to be on the safe side
-	die;
+    die;
 }
 
 return array(
-	'server_url'                   => ( is_ssl() ) ? 'https://www.kaltura.com' : 'http://www.kaltura.com',
-	'cdn_url'                      => ( is_ssl() ) ? 'https://cdn.kaltura.com' : 'http://cdn.kaltura.com',
-	'anonymous_user_id'            => 'Anonymous',
-	'kcw_ui_conf_id_admin'         => 15333782,
-	'categoriesRootId'             => 0,
-	'thumbnail_player_ui_conf_id'  => 14969192,
-	'kaltura_default_player_type'  => 11958362, // default player if player was not chosen in plugin admin screen
-	'kaltura_show_media_from'	   => 'all_account', // when browsing media - should the media be from the whole account or just the logged in user's?
-	'kaltura_enable_kcw'           => false, // enables the legacy flash uploader instead of html5 uploader
-	'kaltura_user_identifier'      => 'user_login',
+    'server_url'                   => ( is_ssl() ) ? 'https://www.kaltura.com' : 'http://www.kaltura.com',
+    'cdn_url'                      => ( is_ssl() ) ? 'https://cdn.kaltura.com' : 'http://cdn.kaltura.com',
+    'anonymous_user_id'            => 'Anonymous',
+    'kcw_ui_conf_id_admin'         => 15333782,
+    'categoriesRootId'             => 0,
+    'thumbnail_player_ui_conf_id'  => 14969192,
+    'kaltura_default_player_type'  => 11958362, // default player if player was not chosen in plugin admin screen
+    'kaltura_show_media_from'      => 'all_account', // when browsing media - should the media be from the whole account or just the logged in user's?
+    'kaltura_enable_kcw'           => false, // enables the legacy flash uploader instead of html5 uploader
+    'kaltura_user_identifier'      => 'user_login',
 );
