480611fb51faed7d7c716b37c5528c27728dfa76
Muli Dayan <muli.dayan@kaltura.com>
2016-09-01 16:51:55 +0300

    version bump - 2.6.4

--- all-in-one-video-pack.php
+++ all-in-one-video-pack.php
@@ -3,14 +3,14 @@
 Plugin Name: All in One Video Pack
 Plugin URI: http://www.kaltura.org/
 Description: This is not just another video embed tool - it includes every functionality you might need for video and rich-media, including playing and uploading.
-Version: 2.6.3
+Version: 2.6.4
 Author: Kaltura
 Author URI: http://www.kaltura.org/
 */
 
 define( 'KALTURA_PLUGIN_FILE', __FILE__ );
 define( 'KALTURA_ROOT', dirname( __FILE__ ) );
-define( 'KALTURA_PLUGIN_VERSION', '2.6.3' );
+define( 'KALTURA_PLUGIN_VERSION', '2.6.4' );
 
 require_once( KALTURA_ROOT . '/lib/Kaltura/Autoloader.php' );
 
