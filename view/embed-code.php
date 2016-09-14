6404acf21e1e94c83df7c6cecd03bdc83dfac198
Muli Dayan <muli.dayan@gmail.com>
2016-09-14 09:21:22 +0300

    WP-99 margin-bottom should be -36px only if there are no hovering controls

--- view/embed-code.php
+++ view/embed-code.php
@@ -34,7 +34,7 @@ $scriptSrc      = KalturaHelpers::getServerUrl() . '/p/' . KalturaHelpers::getOp
 <div style="padding-bottom: <?php echo $hoveringControls ? '36px' : '0'; ?>;">
 	<div style="width: 100%;display: inline-block;position: relative;">
 		<div style="margin-top: <?php echo $height; ?>%;"></div>
-		<div id="<?php echo esc_attr($playerId); ?>" style="position:absolute;top:0;left: 0;right: 0;bottom:<?php echo $hoveringControls ? '-36px' : '0';?>;"></div>
+		<div id="<?php echo esc_attr($playerId); ?>" style="position:absolute;top:0;left: 0;right: 0;bottom:<?php echo ! $hoveringControls ? '-36px' : '0';?>;"></div>
 	</div>
 	<div class="kaltura-powered-by">
 		<div>
